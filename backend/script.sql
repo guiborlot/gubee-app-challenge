34666b70a24d283958dbe5be3d025a483dbb5727
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-02-24 14:23:57 -0300

    Updating script.sql

--- backend/script.sql
+++ backend/script.sql
@@ -36,22 +36,66 @@ values (null, 'Java');
 insert into technology (TechId, Name)
 values (null, 'Angular');
 
+insert into technology (TechId, Name)
+values (null, 'Kotlin');
+
+insert into technology (TechId, Name)
+values (null, 'Redis');
+
+insert into technology (TechId, Name)
+values (null, 'Docker');
+
 insert into market (MarketId, Name)
 values (null, 'Imobiliário');
 
 insert into market (MarketId, Name)
 values (null, 'Móveis');
 
+insert into market (MarketId, Name)
+values (null, 'Materiais de Construção');
+
+insert into market (MarketId, Name)
+values (null, 'Marketplace');
+
 insert into product (ProductId, Name, Description, MarketId)
 values (null, 'MadeiraMadeira', 'integração com a MadeiraMadeira', 2);
+
+insert into product (ProductId, Name, Description, MarketId)
+values (null, 'Americanas', 'integração com a Americanas', 4);
+
+insert into product (ProductId, Name, Description, MarketId)
+values (null, 'Amazon', 'integração com a Amazon', 4);
+
+insert into product (ProductId, Name, Description, MarketId)
+values (null, 'Telhanorte', 'integração com a Telhanorte', 3);
 
 insert into productTechRelation (ProductId, TechId)
 values (1, 1);
 insert into productTechRelation (ProductId, TechId)
 values (1, 2);
-
+insert into productTechRelation (ProductId, TechId)
+values (2, 1);
+insert into productTechRelation (ProductId, TechId)
+values (2, 2);
+insert into productTechRelation (ProductId, TechId)
+values (2, 3);
+insert into productTechRelation (ProductId, TechId)
+values (2, 4);
+insert into productTechRelation (ProductId, TechId)
+values (2, 5);
+insert into productTechRelation (ProductId, TechId)
+values (3, 3);
+insert into productTechRelation (ProductId, TechId)
+values (3, 4);
+insert into productTechRelation (ProductId, TechId)
+values (3, 5);
+insert into productTechRelation (ProductId, TechId)
+values (4, 3);
+insert into productTechRelation (ProductId, TechId)
+values (4, 2);
 select product.ProductId, product.Name, product.Description, market.Name, technology.Name
 from product
          left join productTechRelation on product.ProductId = productTechRelation.ProductId
          left join technology on productTechRelation.TechId = technology.TechId
-         inner join market on product.MarketId = market.MarketId;
+         inner join market on product.MarketId = market.MarketId
+order by ProductId;
