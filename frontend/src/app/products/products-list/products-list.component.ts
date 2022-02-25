de95d5aaccc5f532b219dd74ef173466dbdfdd72
Guilherme Borlot <guiborlot@edu.unifil.br>
2022-02-25 15:16:13 -0300

    listing all technologies checkbox

--- frontend/src/app/products/products-list/products-list.component.ts
+++ frontend/src/app/products/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable, tap } from "rxjs";
 import { ProductsService } from "../products.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
     selector: "app-products-list",
@@ -8,8 +9,8 @@ import { ProductsService } from "../products.service";
     styleUrls: ["./products-list.component.scss"],
 })
 export class ProductsListComponent implements OnInit {
-    
-    dataSource$!: Observable<any>;
+
+    dataSource$!: MatTableDataSource<any>;
 
     displayedColumns: string[] = [
         "id",
@@ -19,11 +20,47 @@ export class ProductsListComponent implements OnInit {
         "technologies"
     ];
 
-    technologies!: string[];
+    technologies: any = [];
+    technologiesChecked: any = [];
+
 
     constructor(private service: ProductsService) {}
 
     ngOnInit(): void {
-        this.dataSource$ = this.service.list();
+        this.service.list().subscribe(res => {
+          this.dataSource$ = res;
+          this.loadAllTechnologies(res);
+        });
     }
+
+    loadAllTechnologies(products: any){
+      let allTechs: any = [];
+      products.forEach((product:any) => {
+        product.technologies.forEach((tech:string) => {
+          allTechs.push(tech);
+        })
+      })
+      this.loadUniqueTechnologies(allTechs);
+    }
+
+    loadUniqueTechnologies(allTechs: string[]){
+      allTechs.forEach((tech:any) => {
+        if(!this.technologies.includes(tech)) {
+          this.technologies.push(tech);
+        }
+      });
+    }
+
+    filter(technology: string, event: any){
+      if(!this.technologiesChecked.find((element:string) => element == technology) && event.checked){
+        this.technologiesChecked.push(technology)
+      } else {
+        this.technologiesChecked = this.technologiesChecked.filter((item:string) => item!=technology);
+      }
+      console.log(this.technologiesChecked)
+    }
+
+
+
+
 }
--- frontend/src/app/products/products.module.ts
+++ frontend/src/app/products/products.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { MatCardModule } from '@angular/material/card'
 import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 import { ProductsListComponent } from './products-list/products-list.component';
 
 
@@ -13,7 +15,9 @@ import { ProductsListComponent } from './products-list/products-list.component';
     CommonModule,
     ProductsRoutingModule,
     MatCardModule,
-    MatTableModule
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule
   ]
 })
 export class ProductsModule { }
